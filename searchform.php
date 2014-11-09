6907b9a200f3607ae6d8ed09edd63799bac60316
John Woodruff <johnwoodruff91@gmail.com>
2014-11-08 21:18:26 -0700

    Initial Commit

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<div>
+		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+		<button type="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
+		<!-- <input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" /> -->
+	</div>
+</form>
